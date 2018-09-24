f5bc90713b991dc2fd16123049c94635229fa95b
EC2 Default User <ec2-user@ip-172-31-38-180.us-east-2.compute.internal>
2018-09-24 05:33:05 +0000

    date difference not working/ sorting not started

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -1,7 +1,15 @@
+var DummyTasks=[
+  {title:"task one", start:Date("2018-01-01T01:00:00"), end:Date("2018-12-31T23:59:00"), reoccuring:"never", priority:3, estTime:10, participatingUsers:[]},
+  {title:"task two", start:Date("2018-01-01T01:00:00"), end:Date("2018-01-01T09:00:00"), reoccuring:"never", priority:1, estTime:10, participatingUsers:[]},
+  {title:"task three", start:Date("2018-01-01T01:00:00"), end:Date("2018-01-01T09:00:00"), reoccuring:"never", priority:3, estTime:10, participatingUsers:[]}
+]
+
+
+
 $(document).ready(function(){
   $("#createTask").click(function(){
       var task = {title:$("#taskTitle").val(),start:$("#start").val(), end:$("#end").val(), reoccuring:$("#reoccuring").val()};
-      jobj = JSON.stringify(task);
+      var jobj = JSON.stringify(task);
       $("#json").text(jobj);
       console.log(jobj);
       
@@ -12,8 +20,23 @@ $(document).ready(function(){
           data: jobj,
           contentType: "application/json; charset=utf-8",
           success: function(data,textStatus) {
-               $("#taskAdded").html(textStatus);
+               
+               var taskListHTML = "<ul>";
+               for( var i in DummyTasks)
+               {
+                 let t = DummyTasks[i];
+                 var timeRemaining = 0;
+                 console.log(t.start - t.end);
+                 console.log("time :"+timeRemaining);
+                 taskListHTML += "<li> "+t.title+" - Time Remaining: "+ timeRemaining+" </li>";                
+               }
+               taskListHTML+="</ul>"
+                $("#taskList").html(taskListHTML);
+               
+            
           }
         })
       });
 });
+
+
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,12 @@ var mongoose = require('mongoose');
 //connect to mongo
 // mongoose.connect('mongodb://localhost/taskDB', { useNewUrlParser: true });
 
-//create schema.
-//Date is both date and time\
-//Priority is an int between 1-3 (1 being the highest)
-//est time is and int of est. hours
-//part. Users is a list of usernames.
+/*create schema.
+Date is both date and time\
+Priority is an int between 1-3 (1 being the highest)
+est time is and int of est. hours
+part. Users is a list of usernames. */
+
 var taskSchema = mongoose.Schema({ 
 title: String,
 start: Date, 
@@ -33,14 +34,15 @@ db.once('open', function() {
 //post task to db
 router.post('/task', function(req, res, next) {
     console.log("in post function");
-    var reqTask= req.body;
-    var newTask = new task({title: reqTask.title, start:reqTask.start, end: reqTask.end, priority:1, estTime:1, participatingUsers:[]});
-    console.log(newTask);
-    // newTask.save(function(err, post) { //[4]
-    //     if (err) return console.error(err);
-    //     console.log(post);
-    //     res.sendStatus(200);
-    // });
+    // var reqTask= req.body;
+    // var newTask = new task({title: reqTask.title, start:reqTask.start, end: reqTask.end, priority:1, estTime:1, participatingUsers:[]});
+    // console.log(newTask);
+    // // newTask.save(function(err, post) { //[4]
+    // //     if (err) return console.error(err);
+    // //     console.log(post);
+    //      res.sendStatus(200);
+    // // });
+    res.sendStatus(200);
 });
 
 module.exports = router;
