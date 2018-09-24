9288492461142d4e99f2e506b6f6311f30a65bc8
EC2 Default User <ec2-user@ip-172-31-38-180.us-east-2.compute.internal>
2018-09-24 21:16:32 +0000

    got mongo, and time working, still working on form validation

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -1,42 +1,68 @@
 var DummyTasks=[
-  {title:"task one", start:Date("2018-01-01T01:00:00"), end:Date("2018-12-31T23:59:00"), reoccuring:"never", priority:3, estTime:10, participatingUsers:[]},
-  {title:"task two", start:Date("2018-01-01T01:00:00"), end:Date("2018-01-01T09:00:00"), reoccuring:"never", priority:1, estTime:10, participatingUsers:[]},
-  {title:"task three", start:Date("2018-01-01T01:00:00"), end:Date("2018-01-01T09:00:00"), reoccuring:"never", priority:3, estTime:10, participatingUsers:[]}
+  {title:"task one", start: new Date("2018-01-01T01:00:00"), end: new Date("2018-12-31T23:59:00"), reoccuring:"never", priority:3, estTime:10, participatingUsers:[]},
+  {title:"task two", start:new Date("2018-01-01T01:00:00"), end: new Date("2018-01-01T09:00:00"), reoccuring:"never", priority:1, estTime:10, participatingUsers:[]},
+  {title:"task three", start: new Date("2018-01-01T01:00:00"), end: new Date("2018-01-01T09:00:00"), reoccuring:"never", priority:3, estTime:10, participatingUsers:[]}
 ]
 
 
 
 $(document).ready(function(){
   $("#createTask").click(function(){
-      var task = {title:$("#taskTitle").val(),start:$("#start").val(), end:$("#end").val(), reoccuring:$("#reoccuring").val()};
-      var jobj = JSON.stringify(task);
-      $("#json").text(jobj);
-      console.log(jobj);
-      
-      var url = "task";
-        $.ajax({
-          url:url,
-          type: "POST",
-          data: jobj,
-          contentType: "application/json; charset=utf-8",
-          success: function(data,textStatus) {
-               
-               var taskListHTML = "<ul>";
-               for( var i in DummyTasks)
-               {
-                 let t = DummyTasks[i];
-                 var timeRemaining = 0;
-                 console.log(t.start - t.end);
-                 console.log("time :"+timeRemaining);
-                 taskListHTML += "<li> "+t.title+" - Time Remaining: "+ timeRemaining+" </li>";                
-               }
-               taskListHTML+="</ul>"
-                $("#taskList").html(taskListHTML);
-               
-            
-          }
-        })
-      });
+      //check to see if all form fields have been completed
+      if(!validateForm())
+      {
+          alert("Please Complete All Fields");
+      }
+      else{
+          var task = {title:$("#taskTitle").val(),start:$("#start").val(), end:$("#end").val(), reoccuring:$("#reoccuring").val(), 
+                    numOfReoccurances:$("#numOfReoccurances").val()};
+          var jobj = JSON.stringify(task);
+          $("#json").text(jobj);
+          console.log(jobj);
+          
+          var url = "task";
+            $.ajax({
+              url:url,
+              type: "POST",
+              data: jobj,
+              contentType: "application/json; charset=utf-8",
+              success: function(data,textStatus) {
+                  createListHTML(DummyTasks)
+              }
+            })
+      }
+    });
 });
 
+function validateForm() {
+    console.log("in validate form");
+  var isValid = true;
+  $("form#newTaskForm :input").each(function() {
+    if ( $(this).val() === '')
+        isValid = false;
+        console.log("returning false for :"+ $(this));
+  });
+  return false;
+}
 
+function createListHTML(list){
+                 var taskListHTML = "<ol>";
+               for( var i in list)
+               {
+                 let t = list[i];
+                 var timeRemaining = (t.end- t.start) / (1000*60*60);
+                 taskListHTML += "<li> "+t.title+" - Due in: "+ timeRemaining+" hrs </li>";                
+               }
+               taskListHTML+="</ol>"
+                $("#taskList").html(taskListHTML);
+}
+
+function displayReoccuringMenu(){
+    var timeMeasurment = document.getElementById("reoccuring").value;
+    if(timeMeasurment == "never")
+    {menuHtml =""}
+    else{
+        var menuHtml= '<label>For how many '+ timeMeasurment+ 's: </label> <input type="number" name="numOfReoccurances"/>'
+    }
+     $("#reoccuringMenu").html(menuHtml);
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 //connect to mongo
-// mongoose.connect('mongodb://localhost/taskDB', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/taskDB', { useNewUrlParser: true });
 
 /*create schema.
 Date is both date and time\
@@ -34,15 +34,14 @@ db.once('open', function() {
 //post task to db
 router.post('/task', function(req, res, next) {
     console.log("in post function");
-    // var reqTask= req.body;
-    // var newTask = new task({title: reqTask.title, start:reqTask.start, end: reqTask.end, priority:1, estTime:1, participatingUsers:[]});
-    // console.log(newTask);
-    // // newTask.save(function(err, post) { //[4]
-    // //     if (err) return console.error(err);
-    // //     console.log(post);
-    //      res.sendStatus(200);
-    // // });
-    res.sendStatus(200);
+    var reqTask= req.body;
+    var newTask = new task({title: reqTask.title, start:reqTask.start, end: reqTask.end, priority:1, estTime:1, participatingUsers:[]});
+    console.log(newTask);
+    newTask.save(function(err, post) { //[4]
+        if (err) return console.error(err);
+        console.log(post);
+         res.sendStatus(200);
+    });
 });
 
 module.exports = router;
